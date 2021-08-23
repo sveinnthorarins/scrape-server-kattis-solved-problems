26c954b67b65228a6d1cb667d80588d65336a1ab
sveinnthorarins <sveinnthz@gmail.com>
2021-08-23 04:10:36 +0000

    removed unneeded header from responses

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ let solvedProblems;
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
